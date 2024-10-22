b8ffe617ffb739820b96acfcdf7f97a20c4f29f0
enesahhmet <enes_ahmet1995@hotmail.com>
2024-10-22 20:24:00 +0300

    Add project files.

---
+++ c#_ders6_diziler/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__ders6_diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region temel dizi örnekleri
+           
+            string[] isimler = new string[5];
+
+            isimler[0] = "Ahmet";
+            isimler[1] = "Çınar";
+            isimler[2] = "Ada";
+            isimler[3] = "Yiğit";
+            isimler[4] = "Sena";
+
+            int[] numaralar = new int[5];
+
+            numaralar[0] = 101;
+            numaralar[1] = 102;
+            numaralar[2] = 103;
+            numaralar[3] = 104;
+            numaralar[4] = 105;
+
+
+
+
+            #endregion
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ ders_6_diziler/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders_6_diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region temel dizi örnekleri
+
+            //2.4.6.8
+            //sarı,kırmızı,mavi,turunucu,beyaz
+            // adana,ankara,istanbul,bursa
+            //Değişken Türü [] DiziAdı =new Değişken Türü[Eleman]
+
+
+            //string[] colors = new string[4];
+            //colors[0] = "Kırmızı";
+            //colors[1] = "Sarı";
+            //colors[2] = "Beyaz";
+            //colors[3] = "Mavi";
+            //Console.WriteLine(colors[2]);
+
+            //string[] cities = new string[5];
+            //cities[0] = "Milona";
+            //cities[1] = "Budapeşte";
+            //cities[2] = "Lyon";
+            //cities[3] = "Kahire";
+            //cities[4] = "Üsküp";
+            //Console.WriteLine(cities[5]);
+
+            //int[] numbers = new int[10];
+            //numbers[0] = 50;
+            //numbers[1] = 48;
+            //numbers[2] = 698;
+            //numbers[3] = 24;
+            //numbers[7] = 748;
+            //Console.WriteLine(numbers[5]);
+            //string[] cities = { "Prag", "Roma", " Atina", "Ankara", "Bursa" };
+            //Console.WriteLine(cities[2]);
+
+            #endregion
+            #region Dizideki Tüm Elemanları Listeleme
+            //string[] colors = {"Sarı","Kırmızı","Beyaz","Mavi","Yeşil","Turuncu","Pembe" };
+            //for (int i = 0; i < colors.Length; i++) 
+            //{ 
+            //Console.WriteLine(colors[i]);
+            
+            //}
+            
+            #endregion
+
+
+
+            Console.Read();
+            
+        }
+    }
+}
